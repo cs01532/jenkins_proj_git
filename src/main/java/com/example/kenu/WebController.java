caeadcef3fbdfe9171ad483192b0370f6fb017c2
cs01532 <40909925+cs01532@users.noreply.github.com>
2021-11-22 22:40:34 +0900

    Update WebController.java

--- src/main/java/com/example/kenu/WebController.java
+++ src/main/java/com/example/kenu/WebController.java
@@ -11,7 +11,7 @@ public class WebController {
     @ResponseBody
     public String main() {
         return "<a href=\"https://infoq.com\">infoq.com</a><br>" 
-            +"<a href=\"https://naver.com\">naver.com!!!</a>"
+            +"<a href=\"https://naver.com\">naver.com!!!</a><br>"
             +"<a href=\"https://daum.net\">daum.net!!</a>";
     }
 }
